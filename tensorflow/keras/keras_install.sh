0398c3f6998a7a46e8e31cda7e0681961bffa3c2
unknown <564615069@qq.com>
2018-01-18 15:58:52 +0800

    update

--- tensorflow/keras/keras_install.sh
+++ tensorflow/keras/keras_install.sh
@@ -3,4 +3,11 @@
 #keras是基于tensorflow的所以要先安装tensorflow。
 
 #安装keras
+sudo apt-get install -y libsm6 libxext6 libxrender1 libxext-dev python3-tk 
+
+
 pip install keras
+pip install pandas
+pip install opencv-python
+pip install Pillow
+pip install h5py
